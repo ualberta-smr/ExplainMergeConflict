a28e78e854e317f5f71ddbe4daf9f0bc87a8d360
kpatenio <patenio@ualberta.ca>
2020-08-27 14:56:30 -0700

    MergeConflictService should now instantiate head and merge_head branch names as static attributes.

--- src/main/java/org/ualberta/smr/explainmergeconflict/services/MergeConflictService.java
+++ src/main/java/org/ualberta/smr/explainmergeconflict/services/MergeConflictService.java
@@ -6,6 +6,11 @@ import com.intellij.openapi.components.ServiceManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vcs.VcsException;
 import git4idea.GitUtil;
+import git4idea.branch.GitBranchUtil;
+import git4idea.commands.Git;
+import git4idea.commands.GitCommand;
+import git4idea.commands.GitCommandResult;
+import git4idea.commands.GitLineHandler;
 import git4idea.history.GitHistoryUtils;
 import git4idea.repo.GitConflict;
 import git4idea.repo.GitRepository;
@@ -21,14 +26,25 @@ import java.util.Objects;
 @Service
 public final class MergeConflictService implements Disposable {
     private static final HashMap<String, ConflictFile> conflictFilesMap = new HashMap<>();
+    private static String headBranchName;
+    private static String mergeBranchName;
     private static String baseRevId;
     private final Project project;
 
     public MergeConflictService(Project project) {
         this.project = project;
+        init();
+    }
 
+    public static MergeConflictService getInstance(@NotNull Project project) {
+        return ServiceManager.getService(project, MergeConflictService.class);
+    }
+
+    private void init() {
+        GitRepository repo = Objects.requireNonNull(Utils.getCurrentRepository(project));
+
+        // Common ancestor
         try {
-            GitRepository repo = Objects.requireNonNull(Utils.getCurrentRepository(project));
             baseRevId = Objects.requireNonNull(GitHistoryUtils.getMergeBase(
                     project,
                     repo.getRoot(),
@@ -37,10 +53,44 @@ public final class MergeConflictService implements Disposable {
         } catch (VcsException e) {
             e.printStackTrace();
         }
+
+        // Branches
+        setBranches(repo);
+
+        // Conflict Files
+        List<GitConflict> conflicts = new ArrayList<>(repo.getStagingAreaHolder()
+                .getAllConflicts()
+        );
+
+        for (GitConflict conflict: conflicts) {
+            ConflictFile conflictFile = new ConflictFile(
+                    conflict.getRoot(),
+                    conflict.getFilePath(),
+                    conflict.getStatus(GitConflict.ConflictSide.OURS),
+                    conflict.getStatus(GitConflict.ConflictSide.THEIRS)
+            );
+            conflictFilesMap.put(conflict.getFilePath().toString(), conflictFile);
+        }
     }
 
-    public static MergeConflictService getInstance(@NotNull Project project) {
-        return ServiceManager.getService(project, MergeConflictService.class);
+    private void setBranches(GitRepository repo) {
+        headBranchName = GitBranchUtil.getBranchNameOrRev(repo);
+
+        /*
+         * Run `git log <ref> -1 --pretty=%D` to get the branch name assigned to the current ref. %D returns the
+         * branch name of a ref without the "()" brackets surrounding it.
+         * Reference: https://git-scm.com/docs/git-log#_pretty_formats
+         */
+        GitLineHandler h = new GitLineHandler(project, repo.getRoot(), GitCommand.LOG);
+        h.addParameters(GitUtil.MERGE_HEAD);
+        h.addParameters("-1");
+        h.addParameters("--pretty=%D");
+        GitCommandResult result = Git.getInstance().runCommand(h);
+        List<String> output = result.getOutput();
+
+        assert output.size() == 1;
+
+        mergeBranchName = output.get(0);
     }
 
     /**
@@ -69,6 +119,14 @@ public final class MergeConflictService implements Disposable {
         return baseRevId;
     }
 
+    public static String getHeadBranchName() {
+        return headBranchName;
+    }
+
+    public static String getMergeBranchName() {
+        return mergeBranchName;
+    }
+
     public HashMap<String, ConflictFile> getConflictFiles() {
         return conflictFilesMap;
     }
@@ -78,6 +136,7 @@ public final class MergeConflictService implements Disposable {
      */
     @Override
     public void dispose() {
+        conflictFilesMap.clear();
         UIController.updateToolWindowAfterNonConflictState(Utils.getCurrentRepository(project));
     }
 }
